0f28299c9d00a366639aa4324a1f6b4bb75cb59a
Mwizzard <toorn@desktop.local>
2025-01-13 19:33:37 -0500

    added simple cpp file

---
+++ git_test.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file git_test.cpp
+ * @author mWizzard (merlinsuter@outlook.com)
+ * @brief test github
+ * @version 0.1
+ * @date 2025-01-13
+ * 
+ * 
+ */
+
+#include <iostream>
+#include <cmath>
+
+int main(){
+    std::cout << "Hello, World !\n";
+}
